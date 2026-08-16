37f96764d5476b35cef45041ccd3bf1a8b31404e
Jorge deboli <jorgedeboli@gmail.com>
2026-08-16 18:23:49 +0200

    test

--- camera/api/guardar_token.php
+++ camera/api/guardar_token.php
@@ -34,7 +34,6 @@ try {
     $tipo_qr = trim($data['tipo_qr']);
     $id_item = (int) $data['id_item'];
     $token = trim($data['token']);
-    $id_sucursal = isset($data['id_sucursal']) ? (int) $data['id_sucursal'] : 0;
 
     $tipos_validos = array('cliente', 'lote', 'gasto', 'gasto_prueba', 'renovacion', 'adelanto', 'articulo', 'venta', 'articulo_venta', 'adelanto_venta', 'plazo_venta', 'autorizar_gasto', 'ia_chat', 'documento_ocr', 'factura_ocr', 'traspaso');
     if (!in_array($tipo_qr, $tipos_validos, true)) {
@@ -47,27 +46,51 @@ try {
     if ($token === '') {
         throw new Exception('Token no puede estar vacío');
     }
-    // En este ERP las fotos de cliente no van por sucursal.
-    if ($tipo_qr === 'cliente') {
-        $id_sucursal = 0;
-    } elseif ($id_sucursal <= 0) {
-        throw new Exception('ID de sucursal no válido');
-    }
 
     $conexion = conectar_bd();
+    if (!$conexion) {
+        throw new Exception('Sin conexión a la base de datos');
+    }
+
+    $rel_id_empresa = 0;
+    if ($tipo_qr === 'cliente') {
+        $stmt_cli = mysqli_prepare($conexion, 'SELECT rel_id_empresa FROM clientes WHERE id_cliente = ? LIMIT 1');
+        if ($stmt_cli) {
+            mysqli_stmt_bind_param($stmt_cli, 'i', $id_item);
+            mysqli_stmt_execute($stmt_cli);
+            $res_cli = mysqli_stmt_get_result($stmt_cli);
+            $row_cli = $res_cli ? mysqli_fetch_assoc($res_cli) : null;
+            mysqli_stmt_close($stmt_cli);
+            $rel_id_empresa = (int) ($row_cli['rel_id_empresa'] ?? 0);
+        }
+    }
+    if ($rel_id_empresa <= 0 && defined('APP_ID')) {
+        $rel_id_empresa = (int) APP_ID;
+    }
+    if ($rel_id_empresa <= 0) {
+        throw new Exception('No se pudo determinar la empresa del token');
+    }
 
     $state_token = 'true';
-    $query = "
+    $query = '
         INSERT INTO tokens_actions
-        (token_string, state_token, id_item, type_item, sucursal_token, fecha_token)
+        (token_string, state_token, id_item, type_item, rel_id_empresa, fecha_token)
         VALUES (?, ?, ?, ?, ?, CURDATE())
-    ";
+    ';
 
     $stmt = mysqli_prepare($conexion, $query);
-    mysqli_stmt_bind_param($stmt, 'ssisi', $token, $state_token, $id_item, $tipo_qr, $id_sucursal);
+    if (!$stmt) {
+        throw new Exception('Error al preparar el token: ' . mysqli_error($conexion));
+    }
+    mysqli_stmt_bind_param($stmt, 'ssisi', $token, $state_token, $id_item, $tipo_qr, $rel_id_empresa);
 
     if (!mysqli_stmt_execute($stmt)) {
-        throw new Exception('Error al guardar el token: ' . mysqli_stmt_error($stmt));
+        $err = mysqli_stmt_error($stmt);
+        if ($err === '') {
+            $err = mysqli_error($conexion);
+        }
+        mysqli_stmt_close($stmt);
+        throw new Exception('Error al guardar el token: ' . $err);
     }
 
     $id_token_insertado = mysqli_insert_id($conexion);
