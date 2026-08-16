645e5a57177b4eda66acd62cf640a550ac78e5b0
Jorge deboli <jorgedeboli@gmail.com>
2026-08-16 09:47:47 +0200

    test

--- servicios.php
+++ servicios.php
@@ -1,1 +1,4 @@
-<?php require_once 'include/session.php'; require_once 'parts/universal/main_files.php'; ?>
+<?php
+require_once 'include/session.php';
+require_once 'parts/universal/main_files.php';
+?>
