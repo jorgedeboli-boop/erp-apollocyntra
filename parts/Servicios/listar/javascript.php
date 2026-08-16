489d7dd9f979a192e489fa9a2d822b48adaaa7b5
Jorge deboli <jorgedeboli@gmail.com>
2026-08-16 09:29:38 +0200

    test

--- parts/Servicios/listar/javascript.php
+++ parts/Servicios/listar/javascript.php
@@ -1,4 +1,1 @@
-<?php
-$vTablesDatatablesLoad = filemtime(__DIR__ . '/tables-datatables-load.js');
-?>
-<script src="parts/servicios/listar/tables-datatables-load.js?v=<?php echo $vTablesDatatablesLoad; ?>"></script>
+
